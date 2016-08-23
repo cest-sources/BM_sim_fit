5e4631f423765c8634256faf8706f86861df1f3a
mzaiss <altustro@gmail.com>
2016-08-23 17:21:00 +0200

    added extrapolation

--- optimisation/tinv.m
+++ optimisation/tinv.m
@@ -6,7 +6,7 @@ load stud_t
 vecx=stud_t(:,1);
 vec= stud_t(:,[0 0.5 0.75 0.8 0.9 0.95 0.98 0.99 0.998]==percent);
 
-vecii=interp1(vecx,vec,1:5000);
+vecii=interp1(vecx,vec,1:10000,'linear','extrap');
 
  val=squeeze(vecii(dgrfreed));
 
