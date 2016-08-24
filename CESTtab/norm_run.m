87b6d62b89a47675b67173f5b884df998d79a841
mzaiss <altustro@gmail.com>
2016-08-24 18:08:37 +0200

    added normalization

--- CESTtab/norm_run.m
+++ CESTtab/norm_run.m
@@ -1,22 +1,35 @@
-function subtab = norm_run(subtab,offseti,excludi)
-%  Ztab('ucl1','B1_run') = norm_run(Ztab('ucl1','B1_run'),1,0)
+function Zrow = norm_run(Zrow,runstr,offset,excludi)
+%  Ztab('ucl1') = norm_run(Ztab('ucl1'),1,0)
+if ~isempty(offset)
+    
+w=Zrow.offset{1};
+subtab=Zrow{:,runstr};
 
-if nargin<3
+if nargin<4
     excludi=[];
 end;
 
+offseti=find_nearest(w,offset);
+
+
 % normalize
-II=1:numel(subtab{1,1});
+II=1:numel(subtab); % for all run entries
 
 for ii=II
-    if ~isempty(subtab{1,1}{ii})
-        subtab{1,1}{ii}=subtab{1,1}{ii}./subtab{1,1}{ii}(offseti);
-        % exclude
-        subtab{1,1}{ii}(excludi)=[];
+    if ~isempty(subtab{ii})
+        Mnorm=subtab{ii}(offseti);
+        subtab{ii}=subtab{ii}./Mnorm;
+        
+        subtab{ii}(excludi)=[];
+        Zrow.offset{1}(excludi)=[];
     end;
 end;
 
-% exclude
+Zrow{:,runstr}=subtab;
+else
+    warning('offset is empty, nothing was done!');
+end;
+
 
 
 
